e8eadcda491886740432882d10da7f0be2af9dae
taka <test>
2023-06-21 08:25:48 +0900

    Initial Commit

---
+++ swiftUI_XCUITest/swiftUI_XCUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_XCUITestApp.swift
+//  swiftUI_XCUITest
+//
+//  Created by mac on 2023/06/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_XCUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
