d97366ce8132b9af0eee25baf9f589e831bc621e
taka <test>
2023-06-21 08:51:41 +0900

    ファイル追加

--- swiftUI_XCUITest/ContentView.swift
+++ swiftUI_XCUITest/ContentView.swift
@@ -5,22 +5,42 @@
 //  Created by mac on 2023/06/21.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+    @State private var toDetailView = false
+    @State private var name = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Spacer().frame(height: 100)
+                
+                TextField("テキストフィールド", text: $name).accessibility(identifier: "contentview_text")
+                    .padding()
+                
+                Spacer()
+                
+                Button("Go to detail") {
+                    toDetailView = true
+                }
+                .accessibility(identifier: "contentview_button")
+                NavigationLink(
+                    destination: DetailView(),
+                    isActive: $toDetailView,
+                    label: { EmptyView() })
+            }
+            .navigationBarTitle("Content")
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct DetailView: View {
+    var body: some View {
+        VStack {
+            Text("This is detail view")
+                .accessibility(identifier: "detailview_text")
+        }
+        .navigationBarTitle("Detail")
     }
 }
--- swiftUI_XCUITestUITests/swiftUI_XCUITestUITests.swift
+++ swiftUI_XCUITestUITests/swiftUI_XCUITestUITests.swift
@@ -8,34 +8,36 @@
 import XCTest
 
 final class swiftUI_XCUITestUITests: XCTestCase {
+    
+    let app = XCUIApplication()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let textfieldsAccessibilityId = "contentview_text"
+    let buttonAccessibilityId = "contentview_button"
+    let textAccessibilityId = "detailview_text"
+    
+    func testMoveToDetailView() {
+        XCTContext.runActivity(named: "Launch app") { _ in
+            app.launch()
+        }
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
+        XCTContext.runActivity(named: "Text input Test") { _ in
+            let typeTextString = "XCUITestでテキスト自動入力テスト"
+            app.textFields[textfieldsAccessibilityId].tap()
+            app.textFields[textfieldsAccessibilityId].typeText(typeTextString)
+            if let str = app.textFields[textfieldsAccessibilityId].value as? String {
+                XCTAssertTrue(str == typeTextString)
             }
+
+        }
+        
+        XCTContext.runActivity(named: "Check content view") { _ in
+            XCTAssertTrue(app.navigationBars["Content"].exists)
+        }
+        
+        XCTContext.runActivity(named: "Tap button and show detail view") { _ in
+            app.buttons[buttonAccessibilityId].tap()
+            XCTAssertTrue(app.navigationBars["Detail"].exists)
+            XCTAssertTrue(app.staticTexts[textAccessibilityId].exists)
         }
     }
 }
