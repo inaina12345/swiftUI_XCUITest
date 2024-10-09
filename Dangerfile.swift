c34db27cf28db79b8b4efbde0d21c4338ab591e8
inaina12345 <107551125+inaina12345@users.noreply.github.com>
2024-10-09 22:44:48 +0900

    Update Dangerfile.swift

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,1 +1,6 @@
-xcode_summary.report 'swiftUI_XCUITest.xcresult'
+# PRの範囲外のメッセージを無視する
+github.dismiss_out_of_range_messages
+
+xcode_warnings.build_timing_summary = true
+xcode_warnings.analyze_file 'build.log'
+#xcode_summary.report 'swiftUI_XCUITest.xcresult'
